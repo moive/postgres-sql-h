055fe7cec3cac9af40f2e764d68ff751b71019b5
Moises Velasquez <moises007@gmail.com>
2025-12-24 19:16:05 -0500

    add FOREIGN KEY to countrylanguage table

--- class3.sql
+++ class3.sql
@@ -88,3 +88,16 @@ ALTER TABLE city DROP CONSTRAINT fk_countrycode
 -- INSERT DATA
 INSERT INTO country
 		values('AFG', 'Afghanistan', 'Asia', 'Southern Asia', 652860, 1919, 40000000, 62, 69000000, NULL, 'Afghanistan', 'Totalitarian', NULL, NULL, 'AF');
+
+
+-- add foreign key to countrylanguage table
+ALTER TABLE countrylanguage
+  ADD CONSTRAINT fk_countrycode
+  FOREIGN KEY (countrycode)
+  REFERENCES country(code);
+  
+  
+SELECT * FROM countrylanguage;
+
+SELECT * FROM country WHERE code = 'AFG';
+DELETE FROM country WHERE code = 'AFG'
