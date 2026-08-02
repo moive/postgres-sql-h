a7d451a793649e47665ef289717f441d36faa4ae
Moises Velasquez <moises007@gmail.com>
2026-08-01 23:25:11 -0500

    create view comments_per_week

---
+++ views.sql
@@ -0,0 +1,18 @@
+--create view comments_per_week
+CREATE OR REPLACE VIEW comments_per_week AS
+
+SELECT
+  date_trunc('weeks', posts.created_at) AS weeks,
+  COUNT(distinct posts.post_id) AS number_of_posts,
+  SUM(claps.counter) AS total_claps,
+  COUNT(*) AS number_of_claps
+FROM
+  posts
+  INNER JOIN claps ON claps.post_id = posts.post_id
+GROUP BY
+  weeks
+ORDER BY
+  weeks DESC;
+
+SELECT * FROM claps WHERE post_id = 1;
+SELECT * FROM comments_per_week;
