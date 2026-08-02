a1f4ef300d5be10ecded9531d2cf507d58c6f40e
Moises Velasquez <moises007@gmail.com>
2026-08-01 23:40:01 -0500

    create view comments_per_week_mat

--- views.sql
+++ views.sql
@@ -16,3 +16,24 @@ ORDER BY
 
 SELECT * FROM claps WHERE post_id = 1;
 SELECT * FROM comments_per_week;
+
+--create view materialized comments_per_week_mat 
+CREATE MATERIALIZED VIEW comments_per_week_mat AS
+
+SELECT
+  date_trunc('weeks', posts.created_at) AS weeks,
+  COUNT(distinct posts.post_id) AS number_of_posts,
+  SUM(claps.counter) AS total_claps,
+  COUNT(*) AS number_of_claps
+FROM
+  posts
+  INNER JOIN claps ON claps.post_id = posts.post_id
+GROUP BY
+  weeks
+ORDER BY
+  weeks DESC;
+
+SELECT * FROM comments_per_week;
+SELECT * FROM comments_per_week_mat;
+-- for refresh view materialized
+REFRESH MATERIALIZED VIEW comments_per_week_mat;
