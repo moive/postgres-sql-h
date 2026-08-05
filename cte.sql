bf6cdd87eb4606e266c2aca80b916a3ef45ee471
Moises Velasquez <moises007@gmail.com>
2026-08-05 16:05:59 -0500

    create table employees

--- cte.sql
+++ cte.sql
@@ -75,3 +75,27 @@ SELECT
 FROM
   multiplication_table;
 
+--create table employees
+DROP TABLE IF EXISTS "public"."employees";
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS employees_id_seq;
+
+-- Table Definition
+CREATE TABLE "public"."employees" (
+    "id" int4 NOT NULL DEFAULT nextval('employees_id_seq'::regclass),
+    "name" varchar,
+    "reports_to" int4,
+    PRIMARY KEY ("id")
+);
+
+INSERT INTO "public"."employees" ("id", "name", "reports_to") VALUES
+(1, 'Jefe Carlos', NULL),
+(2, 'SubJefe Susana', 1),
+(3, 'SubJefe Juan', 1),
+(4, 'Gerente Pedro', 3),
+(5, 'Gerente Melisa', 3),
+(6, 'Gerente Carmen', 2),
+(7, 'SubGerente Ramiro', 5),
+(8, 'Programador Fernando', 7),
+(9, 'Programador Eduardo', 7),
+(10, 'Presidente Karla', NULL);
