a7ce1d2e7fca22054adf43606ba903443201ae65
Moises Velasquez <moises007@gmail.com>
2026-08-05 14:58:25 -0500

    multiple CTEs

--- cte.sql
+++ cte.sql
@@ -16,3 +16,14 @@ WITH posts_week_2024 AS (
 
 -- SELECT * FROM posts_week_2024 WHERE weeks BETWEEN '2024-01-01' AND '2024-12-31';
 SELECT * FROM posts_week_2024 WHERE weeks BETWEEN '2024-01-01' AND '2024-12-31' AND total_claps >= 600
+
+
+--multiples CTEs
+WITH claps_per_post AS (
+	SELECT post_id, SUM(counter) FROM claps GROUP BY post_id
+), posts_from_2023 AS (
+	SELECT * FROM posts WHERE created_at BETWEEN '2023-01-01' AND '2023-12-01'
+)
+
+SELECT * from claps_per_post
+WHERE claps_per_post.post_id IN (SELECT post_id FROM posts_from_2023)
