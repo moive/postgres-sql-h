1eff24c6037d6b196b56cd254156d36eebdf15ac
Moises Velasquez <moises007@gmail.com>
2026-08-06 01:20:46 -0500

    stored procedure controlled_raise

--- stored-procedures.sql
+++ stored-procedures.sql
@@ -43,3 +43,41 @@ SELECT
   1 AS percentage
 FROM
   employees;
+
+--create stored procedure controlled_raise
+CREATE OR REPLACE PROCEDURE controlled_raise(percentage NUMERIC) AS
+$$
+DECLARE
+	real_percentage NUMERIC(8,2);
+	total_employees int;
+
+BEGIN
+	real_percentage = percentage / 100;
+	--   RAISE NOTICE 'Percentage: %', percentage;
+	-- history
+	INSERT INTO raise_history (date, employee_id, base_salary, amount, percentage)
+	SELECT
+	  current_date AS "date",
+	  employee_id,
+	  salary,
+	  max_raise (employee_id) * real_percentage AS amount,
+	  percentage
+	FROM
+	  employees;
+	  
+	-- updated table employees
+	UPDATE employees
+		SET salary = (max_raise(employee_id) * real_percentage) + salary;
+	 
+	COMMIT;
+	
+	SELECT COUNT(*) INTO total_employees FROM employees;
+	RAISE NOTICE 'Afected % employees', total_employees;
+
+END;
+$$ LANGUAGE plpgsql;
+
+CALL controlled_raise(1);
+
+SELECT * from raise_history;
+SELECT * FROM employees;
