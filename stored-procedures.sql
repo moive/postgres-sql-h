0c611a5e3cccb9ca8164ff1b10bd7ccced4cae01
Moises Velasquez <moises007@gmail.com>
2026-08-06 00:31:10 -0500

    create stored procedure insert_region

--- stored-procedures.sql
+++ stored-procedures.sql
@@ -11,3 +11,24 @@ $$ LANGUAGE plpgsql;
 
 
 SELECT * from country_region();
+
+
+-- create stored procedure
+CREATE
+OR REPLACE PROCEDURE insert_region_proc (int, varchar) AS $$
+BEGIN
+	INSERT INTO regions (region_id, region_name)
+	VALUES($1, $2);
+	
+	RAISE NOTICE 'Variables: %, %', $1, $2;
+	
+-- 	ROLLBACK;
+	COMMIT;
+
+END;
+
+$$ LANGUAGE plpgsql;
+
+CALL insert_region_proc(5, 'Central America');
+
+SELECT * FROM regions;
