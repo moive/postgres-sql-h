786c3ea7d21465f68bec121857ff9a33c812a7c2
Moises Velasquez <moises007@gmail.com>
2026-08-05 22:57:50 -0500

    create function max_raise

--- functions.sql
+++ functions.sql
@@ -12,3 +12,36 @@ $$
 LANGUAGE plpgsql;
 
 SELECT first_name, greet_employee(first_name) FROM employees;
+
+-- without function
+SELECT
+  employee_id,
+  first_name,
+  salary,
+  max_salary,
+  max_salary - salary AS possible_raise
+FROM
+  employees
+  INNER JOIN jobs ON jobs.job_id = employees.job_id;
+
+-- with function
+CREATE OR REPLACE FUNCTION max_raise (empl_id int) 
+RETURNS NUMERIC(8, 2) AS $$
+
+DECLARE
+	possible_raise NUMERIC(8,2);
+
+BEGIN
+
+	SELECT max_salary - salary INTO possible_raise
+	FROM employees
+	INNER JOIN jobs ON jobs.job_id = employees.job_id
+	WHERE employee_id = empl_id;
+	
+	RETURN possible_raise;
+
+END;
+
+$$ LANGUAGE plpgsql;
+
+SELECT employee_id, first_name, max_raise(employee_id) FROM employees;
