ed76923ff325b55bbf8f6c2272eed445ddf53826
Moises Velasquez <moises007@gmail.com>
2026-08-05 23:25:28 -0500

    multiple query and varibles, create functions

--- functions.sql
+++ functions.sql
@@ -45,3 +45,44 @@ END;
 $$ LANGUAGE plpgsql;
 
 SELECT employee_id, first_name, max_raise(employee_id) FROM employees;
+
+
+--multiple queries
+CREATE OR REPLACE FUNCTION max_raise_multiple_variable (empl_id int) 
+RETURNS NUMERIC(8, 2) AS $$
+
+DECLARE
+	employee_job_id int;
+	current_salary NUMERIC(8,2);
+	
+	job_max_salary NUMERIC(8,2);
+	possible_raise NUMERIC(8,2);
+
+BEGIN
+	-- Take the job and the salary
+	SELECT job_id, salary 
+	INTO employee_job_id, current_salary
+	FROM employees
+	WHERE employee_id = empl_id;
+	
+	-- Take the highest salary based on the employee's work
+	SELECT max_salary INTO job_max_salary 
+	FROM jobs 
+	WHERE job_id = employee_job_id;
+	
+	-- Calculations
+	possible_raise = job_max_salary - current_salary;
+	
+	RETURN possible_raise;
+
+END;
+
+$$ LANGUAGE plpgsql;
+
+SELECT
+  employee_id,
+  first_name,
+  max_raise (employee_id),
+  max_raise_multiple_variable(employee_id)
+FROM
+  employees;
